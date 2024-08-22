a12cd79c8c4d57f64712a645ad7c7e9a6bb0c669
naisaika <naisaikapoemelody@gmail.com>
2024-08-22 17:18:07 +0900

    first commit

---
+++ src/pages/MuiTest.js
@@ -0,0 +1,12 @@
+import MuiButton from "../components/MuiButton" 
+import SignInSide from "../components/Signin"
+
+const MuiTest = () => {
+
+    return(<>
+        <MuiButton />
+        <SignInSide />
+    </>)
+}
+
+export default MuiTest
